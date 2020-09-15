d29d2957fd826e3635f550a8587b6bf39b4939ee
olu oyelaja <oyelajaos@gmail.com>
2020-09-14 20:01:23 -0400

    Changes

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import SearchIcon from "@material-ui/icons/Search";
 
 
 
@@ -10,7 +11,8 @@ function Header() {
             <img className="Img_Logo" src="./logo.png" alt="logo"/>
         </Link>
             <h1 className="Header_Text">SOKO ...all African and Carribean</h1>
-        
+        <input type="text" className="Header_Input"/>
+        <SearchIcon className=""
     
     </nav>
  
