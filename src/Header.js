a9334ea3e8b4e038b2597113f3a2daaa8ff26af6
olu oyelaja <oyelajaos@gmail.com>
2020-09-14 20:07:24 -0400

    Changes

--- src/Header.js
+++ src/Header.js
@@ -6,17 +6,18 @@ import SearchIcon from "@material-ui/icons/Search";
 
 
 function Header() {
-    return <nav className="Header">
+    return (
+    <nav className="Header">
         <Link to="/">
             <img className="Img_Logo" src="./logo.png" alt="logo"/>
         </Link>
             <h1 className="Header_Text">SOKO ...all African and Carribean</h1>
         <input type="text" className="Header_Input"/>
-        <SearchIcon className="Header_Search />"
+        <SearchIcon className="Header_Search" />
     
     </nav>
  
- 
+    );
 }
 
 export default Header;
