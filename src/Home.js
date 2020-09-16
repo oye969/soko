032dfd08a505c9b6e11d51c5bb93434122b004c5
olu oyelaja <oyelajaos@gmail.com>
2020-09-16 08:04:12 -0400

    Chnages

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ function Home() {
             id="123245"
             title="Wheat Flour"
             price={5.99}
-            rating={5}
+            rating={4}
             image ="https://goodeggs2.imgix.net/product_photos/nnVTRjdTy3zIiaaSSx7A_2017_0926_KingArthur_OrganicWholeWheatFlour_2lbs%20%28b4cbfcae34e21fd306172bd53d691301573b7078%29.jpg?w=840&h=525&fm=jpg&q=80&fit=crop"
             />
             
