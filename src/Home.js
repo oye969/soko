918665362878ef44a8ca7febf0d0079dd4b0c822
olu oyelaja <oyelajaos@gmail.com>
2020-09-15 23:19:00 -0400

    Changes

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Product from './Product'
 
 function Home() {
     return (
         <div className="Home">
             <img src ="./dd.png"  className="Home_Banner"  alt="Banner" />
+
+            <Product
+            id="123245"
+            title="Wheat Flour"
+            price={5.99}
+            rating={5}
+            image=""
+            />
         </div>
     )
 }
---
+++ src/Product.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Product(
+    { id, title, image, price, rating }
+) {
+    return (
+        <div className="Product">
+            
+        </div>
+    )
+}
+
+export default Product;
